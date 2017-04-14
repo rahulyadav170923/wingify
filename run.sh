3d4e95a3264ba8655a17bd8dba56d56b3aba9a9b
rahulyadav <rahulyadav170923@gmail.com>
2017-04-15 02:40:50 +0530

    made some changes but error of sourcing virtualenv not resolved

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 if [ ! -d $PWD/venv ]; then
-    virtualenv -p python3 venv
+    # virtualenv -p python3 venv
     pip install -r requirements.txt
 fi
-source $PWD/venv/bin/activate
+# source $PWD/venv/bin/activate
 if [ "$1" == 'tests' ]; then
-    python $PWD/tests.py
+    python3 $PWD/tests.py
 elif [ "$1" == 'server' ]; then
-    python $PWD/app.py
+    python3 $PWD/app.py
 else
     echo "Add and argument  (tests or server) "
 fi
