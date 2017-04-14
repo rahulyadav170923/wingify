b19cb1628818e834933a692c475203566dc8bc80
rahulyadav <rahulyadav170923@gmail.com>
2017-04-14 12:05:04 +0530

    error resolved

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ class HTTP_RequestHandler(BaseHTTPRequestHandler):
 def run():
   print('starting server...')
   if 'PORT' in os.environ.keys() :
-    port = os.environ['PORT']
+    port = int(s.environ['PORT'])
   else:
     port = 8000
   server_address = ('0.0.0.0', port )
